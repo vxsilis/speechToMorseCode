16f18df5879ba262edf30dfc54a1b7b8f94a3153
vxsilis <56308691+vxsilis@users.noreply.github.com>
2022-05-13 17:39:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+$(function(){
+
+    $('.scroll').click(function(){
+
+        $('html, body').animate({
+
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 650);
+        return false;
+    });
+
+});
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
